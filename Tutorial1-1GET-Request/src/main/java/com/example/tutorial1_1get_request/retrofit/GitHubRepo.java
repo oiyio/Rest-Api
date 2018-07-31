818e9c8f37eed98d23dbb3cddf1613813ff58d9e
Smart Tool Factory <smarttoolfactory@icloud.com>
2018-07-31 20:43:07 +0300

    Second Commit

---
+++ Tutorial1-1GET-Request/src/main/java/com/example/tutorial1_1get_request/retrofit/GitHubRepo.java
@@ -0,0 +1,14 @@
+package com.example.tutorial1_1get_request.retrofit;
+
+public class GitHubRepo {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Tutorial1-2POST-Request/src/main/java/com/example/restapi/network/APIService.java
@@ -0,0 +1,17 @@
+package com.example.restapi.network;
+
+import com.example.restapi.model.Post;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface APIService {
+
+    @POST("/posts")
+    @FormUrlEncoded
+    Call<Post> savePost(@Field("title") String title,
+                        @Field("body") String body,
+                        @Field("userId") long userId);
+}
